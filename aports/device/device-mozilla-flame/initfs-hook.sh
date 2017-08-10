b7b433f89d00a621ca16aa3d94151110980f4b45
Yuval Adam <_@yuv.al>
2017-08-10 10:54:52 +0300

    Initial /sys updates for flame

--- aports/device/device-mozilla-flame/initfs-hook.sh
+++ aports/device/device-mozilla-flame/initfs-hook.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-# set framebuffer resolution
-cat /sys/class/graphics/fb0/modes > /sys/class/graphics/fb0/mode
+# various kernel parameters taken from
+# https://github.com/mozilla-b2g/device-flame/blob/master/init.target.rc
+echo "100" > /sys/kernel/mm/ksm/pages_to_scan
+echo "500" > /sys/kernel/mm/ksm/sleep_millisecs
+echo "1" > /sys/kernel/mm/ksm/run
+echo "192M" > /sys/block/zram0/disksize
+
+# params to get the screen working
+echo "480,1708" > /sys/devices/virtual/graphics/fb0/virtual_size
+echo "VSYNC=363784349135" > /sys/devices/virtual/graphics/fb0/vsync_event
+echo "panel_power_on = 0" > /sys/devices/virtual/graphics/fb0/show_blank_event
+
