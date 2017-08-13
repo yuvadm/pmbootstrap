b58c4cf1a375a55b3dad27dc2e1f63c1e68e8668
Yuval Adam <_@yuv.al>
2017-08-13 23:39:03 +0300

    Revert "Small initfs change"
    This reverts commit 8800dd54a02252294a255d9c4818a0305c018b58.

--- aports/device/device-mozilla-flame/initfs-hook.sh
+++ aports/device/device-mozilla-flame/initfs-hook.sh
@@ -8,5 +8,6 @@ echo "1" > /sys/kernel/mm/ksm/run
 echo "192M" > /sys/block/zram0/disksize
 
 # params to get the screen working
-echo "480,1708" > /sys/class/graphics/fb0/virtual_size
+echo "480,1708" > /sys/devices/virtual/graphics/fb0/virtual_size
+echo "panel_power_on = 0" > /sys/devices/virtual/graphics/fb0/show_blank_event
 
