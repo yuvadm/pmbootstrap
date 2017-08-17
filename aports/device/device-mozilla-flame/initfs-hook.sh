b0be2eca906138d8f30571bf75723db929a26596
Yuval Adam <_@yuv.al>
2017-08-17 20:20:20 +0300

    wcnss-service changes for Flame

--- aports/device/device-mozilla-flame/initfs-hook.sh
+++ aports/device/device-mozilla-flame/initfs-hook.sh
@@ -3,3 +3,7 @@
 fbdev=/sys/class/graphics/fb0
 echo "480,1708" > $fbdev/virtual_size
 cat $fbdev/modes > $fbdev/mode
+
+echo -n "Firefox OS" > /sys/devices/virtual/android_usb/android0/iManufacturer
+echo -n "Firefox OS" > /sys/devices/virtual/android_usb/android0/iProduct
+echo -n "f30ecc8f" > /sys/devices/virtual/android_usb/android0/iSerial
